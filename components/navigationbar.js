80f90b79eee0d55cad47bcc703dcea23dcbc05f9
TAPUBRAT\tapub <tapubrat.bordoloi@gmail.com>
2026-09-18 23:29:59 +0530

    Included js

--- components/navigationbar.js
+++ components/navigationbar.js
@@ -3,6 +3,9 @@ class classNameNavbar extends HTMLElement {
     this.innerHTML = `
 <nav class="navbar sticky-top navbar-expand-lg my-custom-navbar py-0">
     <div class="container-fluid">
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
